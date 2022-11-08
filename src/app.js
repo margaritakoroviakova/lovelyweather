c67a1794bffcd941f569b074a86cf55921f909d7
margaritakoroviakova <koroviakovam@gmail.com>
2022-11-08 12:10:21 +0200

    API key

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ function mainForecast(response) {
 }
 
 function forecast (coordinates) {
-  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiKey = "b1a8336ff1e05b64da5625e4158fbea3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(mainForecast);
 }
